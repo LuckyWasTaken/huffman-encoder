4e94c8403cdad52cf0d39e97552f0060c14eda41
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-09-11 12:59:57 +0300

    Fixed indentation

--- encoder.cpp
+++ encoder.cpp
@@ -5,38 +5,39 @@
 
 using namespace std;
 
+int main()
+{
+    ifstream input("lorem.txt");
+    map<char, int> freq;
 
-int main () {
-  ifstream input ("lorem.txt");
-  map<char, int> freq;
-    
-
-  if (input.is_open())
-  {
-    char c;
-    while (input.get(c)) {
-      map<char, int>::iterator it = freq.find(c);
-      if(it != freq.end()) {
-        freq[c] = freq[c] + 1;
-      } else {
-        freq[c] = 0;
-      }
-    };          
-    input.close();
-
-
-    multimap<int, char> sorted;    
-    for(map<char, int>::iterator it = freq.begin(); it != freq.end(); it++)
+    if (input.is_open())
     {
-      sorted.insert(make_pair(it->second, it->first));
+        char c;
+        while (input.get(c))
+        {
+            map<char, int>::iterator it = freq.find(c);
+            if (it != freq.end())
+            {
+                freq[c] = freq[c] + 1;
+            }
+            else
+            {
+                freq[c] = 0;
+            }
+        };
+        input.close();
+
+        multimap<int, char> sorted;
+        for (map<char, int>::iterator it = freq.begin(); it != freq.end(); it++)
+        {
+            sorted.insert(make_pair(it->second, it->first));
+        }
     }
-    
-  } else {
-    cout << "Unable to open file"; 
-    return 1;
-  } 
+    else
+    {
+        cout << "Unable to open file";
+        return 1;
+    }
 
-
-
-  return 0;
+    return 0;
 }
