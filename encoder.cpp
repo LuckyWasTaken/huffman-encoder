b887e462de2078421e8c5ca0a0297198a362b4b0
Nikolay Tikhonov <a.for.autism1337@gmail.com>
2018-09-24 12:03:39 +0300

    Done tree building

--- encoder.cpp
+++ encoder.cpp
@@ -2,18 +2,71 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <queue>
 
 using namespace std;
 
+struct Node {
 
-template<class M>
-void print(const M& mmap)
+    char            data;
+    unsigned        freq;
+    Node            *left, *right;
+
+    Node(char data, unsigned freq) {
+        left = right = NULL;
+        this->data = data;
+        this->freq = freq;
+    }
+};
+
+struct compare {
+    bool operator() (Node* l, Node* r) {
+        return (l->freq > r->freq);
+    }
+};
+
+
+void printCodes(struct Node* root, string str)
 {
-    for (auto & e : mmap)
-        std::cout << "{" << e.first << "," << e.second << "} ";
-    std::cout << '\n';
+
+    if (!root)
+        return;
+
+    if (root->data != '$')
+        cout << root->data << ": " << str << "\n";
+
+    printCodes(root->left, str + "0");
+    printCodes(root->right, str + "1");
 }
 
+void encode(multimap<int, char> freqchart) {
+    struct Node *left, *right, *top;
+
+    priority_queue<Node*, vector<Node*>, compare> minHeap;
+
+    for (multimap<int, char>::iterator itr = freqchart.begin(); itr != freqchart.end(); ) {
+        minHeap.push(new Node(itr->second, itr->first));
+        ++itr;
+    }
+
+    while (minHeap.size() != 1) {
+
+        left = minHeap.top();
+        minHeap.pop();
+
+        right = minHeap.top();
+        minHeap.pop();
+
+        top = new Node('$', left->freq + right->freq);
+
+        top->left = left;
+        top->right = right;
+
+        minHeap.push(top);
+    }
+
+    printCodes(minHeap.top(), "");
+}
 
 int main()
 {
@@ -37,12 +90,13 @@ int main()
         };
         input.close();
 
-        multimap<int, char, greater<int>> sorted;
+        multimap<int, char> sorted;
         for (map<char, int>::iterator it = freq.begin(); it != freq.end(); it++)
         {
             sorted.insert(make_pair(it->second, it->first));
         }
-        print(sorted);
+
+        encode(sorted);
     }
     else
     {
